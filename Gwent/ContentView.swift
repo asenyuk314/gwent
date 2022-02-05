67a4d6867538467ced70dff4f36b2e2e0390ed61
Alexander Senyuk <a.senyuk314@gmail.com>
2022-02-05 12:54:25 +0300

    Initial Commit

---
+++ Gwent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gwent
+//
+//  Created by Александр Сенюк on 05.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gwent/GwentApp.swift
@@ -0,0 +1,17 @@
+//
+//  GwentApp.swift
+//  Gwent
+//
+//  Created by Александр Сенюк on 05.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct GwentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
